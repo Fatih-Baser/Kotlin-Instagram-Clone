14fa2834694d8231636a1e5b261f32134ef55bee
fatih40 <baserfatih40@gmail.com>
2021-04-05 12:14:08 +0200

    I edited

--- KotlinInstagramClone/app/src/main/java/com/fatihbaser/kotlininstagramclone/MainActivity.kt
+++ KotlinInstagramClone/app/src/main/java/com/fatihbaser/kotlininstagramclone/MainActivity.kt
@@ -1,22 +1,42 @@
 package com.fatihbaser.kotlininstagramclone
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        FirebaseAuth.getInstance()
+       auth= FirebaseAuth.getInstance()
     }
 
     fun singnInClicked (view: View){
 
     }
     fun singnUpClicked(view: View){
+
+        val email=userEmailText.text.toString()
+        val password =passwordText.text.toString()
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task ->
+
+          if( task.isSuccessful){
+              val intent=Intent(applicationContext,FeedActivity::class.java)
+              startActivity(intent)
+              finish()
+          }
+        }.addOnFailureListener{exception ->
+            if(exception !=null){
+                Toast.makeText(applicationContext,exception.localizedMessage.toString(),Toast.LENGTH_LONG)
+            }
+        }
 
     }
 }
