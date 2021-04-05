f4f8cd79c883bf0d6cf39c095ebba321bda2d676
fatih40 <baserfatih40@gmail.com>
2021-04-05 11:40:45 +0200

    I edited

--- KotlinInstagramClone/app/src/main/java/com/fatihbaser/kotlininstagramclone/MainActivity.kt
+++ KotlinInstagramClone/app/src/main/java/com/fatihbaser/kotlininstagramclone/MainActivity.kt
@@ -2,10 +2,21 @@ package com.fatihbaser.kotlininstagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseAuth.getInstance()
+    }
+
+    fun singnInClicked (view: View){
+
+    }
+    fun singnUpClicked(view: View){
+
     }
 }
